e13e7df019abacef170e464c97e8154521199e2c
Darryl Gould <darryl@thegouldhome.com>
2026-07-15 11:42:56 -0700

    Index change for Live data

--- RollingSQL_ArchiveAddition.py
+++ RollingSQL_ArchiveAddition.py
@@ -147,10 +147,10 @@ def resolve_tables(chunk_start, live_start):
             "acc_log_MTDTL": "clarity_rpt.dbo.ACC_LOG_MTLDTL_IX",
             "acc_WRKF": "clarity_rpt.dbo.ACCESS_WRKF",
             # --- INDEX VARIABLES FOR LIVE (Rowstore B-Tree) ---
-            "index_def_tmpAcc": ", PRIMARY KEY CLUSTERED (ACCESS_TIME, ACCESS_INSTANT, PROCESS_ID)",
+            "index_def_tmpAcc": ",  INDEX ix_tmpAcc CLUSTERED (ACCESS_INSTANT, PROCESS_ID)",
             "index_def_tmpFinal": "", # Keep it heap initially
             # Create a localized B-Tree index post-insert to optimize final sorting
-            "index_def_tmpFinal_post": "CREATE NONCLUSTERED INDEX idx_tmpFinal_perf ON #tmpFinal (ACCESS_TIME, METRIC_ID);"
+            "index_def_tmpFinal_post": "CREATE CLUSTERED INDEX idx_tmpFinal_perf ON #tmpFinal (ACCESS_TIME, METRIC_ID);"
         }
 # ----------------------------
 # CHUNKED DATE RANGE
